06a26e5f3597b27cea05e346961faea5fd9a5540
erdos <erdoskuanyshbek@gmail.com>
2026-05-07 02:38:20 +0500

    CORS for frontend fixed

--- src/main/java/com/seabattle/config/CorsConfig.java
+++ src/main/java/com/seabattle/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //
-                .allowedOriginPatterns("https://seabattle-frontend.vercel.app/")
+                .allowedOriginPatterns("https://seabattle-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
